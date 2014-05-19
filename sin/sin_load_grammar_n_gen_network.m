66dad10631fedd28410781d7c6a4907e7c119ca6
Nam Vo <nam.poke@gmail.com>
2014-05-19 15:09:18 -0400

    update

--- sin/sin_load_grammar_n_gen_network.m
+++ sin/sin_load_grammar_n_gen_network.m
@@ -3,9 +3,8 @@ function sin = sin_load_grammar_n_gen_network( grammar_file, T )
 %SIN_LOAD_GRAMMAR_N_GEN_NETWORK Summary of this function goes here
 %   Detailed explanation goes here
 
-    grammar          = sin_load_grammar('grammar1.txt');
+    grammar          = sin_load_grammar(grammar_file);
     compiled_grammar = sin_compile_grammar( grammar );
-    T                = 100; 
     sin              = sin_alloc_inference_network(compiled_grammar, T);
 
     
